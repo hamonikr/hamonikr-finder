1b70260e16c54dad3867135476a0f3d27757e937
김경곤 <kg1942@invesume.com>
2019-11-12 18:20:09 +0900

    client config file setting update

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const BrowserWindow = electron.BrowserWindow;
 const windowStateKeeper = require('electron-window-state');
 const { globalShortcut } = require("electron");
 const fs = require('fs');
+const mkdirp = require('mkdirp');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readdir);
+const mkDirpAsync = promisify(mkdirp);
 
 const CHILD_PADDING = 100;
 
@@ -48,7 +52,7 @@ function createWindow () {
   	mainWindow.setMenu(null);
 		mainWindow.setMenuBarVisibility(false);	
   // Open the DevTools.
-  	mainWindow.webContents.openDevTools(); 
+  //	mainWindow.webContents.openDevTools(); 
 
 		mainWindow.on('closed', function () {
 		mainWindowState.saveState(mainWindow);
@@ -181,103 +185,136 @@ ipcMain.on('openConfigFile', (event, path) => {
 	});
 });
 
+
+const osType = require('os');
+const dir  = osType.homedir() + '/.config/hamonikr_finder'
+
 ipcMain.on('save-dir-path', (event, arg) => {
-	console.log("save-dir-path----"+ arg);		
-	//FnChk_settingsFile();
-
-	//setTimeout(create_settingFile, 600, arg);
-	
-	//setTimeout(userInfo, 600, '');
-	//setTimeout(watcherCall, 600, '');
-
-	test(arg);
-
+	// 설정 폴더 생성.
+	makeRecursiveFileAsync(dir, arg);
 })
 
+//const makeRecursiveFileAsync = async(data, fileNm) => {
+const makeRecursiveFileAsync = async(dir, arg) => {
+   try{
+//      var json = data;
+//			var osType = require('os');
+//		  var dir  = osType.homedir() + '/.config/hamonikr_finder'
+      //var makeDirpAsync = await mkDirpAsync(dir);
+			//var makeFileTmp = await userInfoWriteFile();
+			//var uuidRestApiChk = await test3();
 
+			// 폴더 및 파일 유무 체크
+			var chkFile = FnChk_settingsFile();
+			console.log("chkFile==="+ chkFile);
+			if( chkFile == false ){
+				var makeDirpAsync = await mkDirpAsync(dir);
+				var makeFileTmp = await userInfoWriteFile();
+			}
 
-async function test( arg ){
-//create_userInfoFile
-
-	await testCreateMkdir();
-
-	await test1();
-	//const uuidTmp = await test2();
-	//console.log("u---"+ uuidTmp);
-	await test3();
-
-}
-
-function testCreateMkdir(){
-     var osType = require('os');
-    var fileDir  = osType.homedir() + '/.config/hamonikr_finder'
-fs.promises.mkdir(fileDir, {recursive: true}).then(x => fs.promises.writeFile(fileDir+"/test.txt", "Hey there!"));
-
-//	return new Promise((resolve, reject) => {
-//	   var osType = require('os');
-//    var fileDir  = osType.homedir() + '/.config/hamonikr_finder2';
-//
-//        fs.mkdir(fileDir, 0o777, function(stats, err) {
-//          if (err) {
-//            return reject(err);
-//          }
-//
-//          return resolve(true);
-//     		});
-//     	}).catch((err) => {
-//     		   logger.error(err);
-//     		   return err;
-//     		 })
+			//파일의 uuid가 디비에 존제 여부
+			var chkUuidInFile = await readUuidFile();
+			//var chkUuidDb = await uuid_db_chk(chkUuidInFile);
+			var chkUuidDb = await getToken(chkUuidInFile);
 			
+			// 색인 폴더 설정
+			var makeWatchFile = await create_settingFile(arg);
+			var es_index = await esIndexExists(chkUuidInFile);
+			console.log("aaaaaaaaaaaaaaaaaa");
+			var watcher_call = await watcherCall(chkUuidInFile);
+			console.log("bbbbbbbbbbbbbbbbbbbb");
+      return "true";
+   }
+   catch(err){
+	 	console.log("error");
+       return Object.assign(err);
+   }
+}
 
- // return new Promise(function(resolve, reject){
- //   var uniqid = require('uniqid');
- //   var osType = require('os');
- //   var fileDir  = osType.homedir() + '/.config/hamonikr_finder2';
- // 	
- //   resolve("async는 Promise방식을 사용합니다.");
- // });
+const request = require('request');
+function uuid_db_chk(arg){
+	return new Promise((resolve, reject) => {
+
+	console.log("dbchk ==== "+ arg);
+
+		const formData = {
+	    userUuid:  arg
+	  };
+		console.log("formdata====" + formData);
+	  request.post({url: "http://127.0.0.1:3001/userinfo", formData: formData}, async (err, response, body) => {
+	    if (err) return reject(err);
+	
+	    const result = body; // JSON.parse(body);
+			console.log("dbchk===result ==="+ result);
+			resolve("true");
+		});
+	});
+}
+
+function readUuidFile(){
+return new Promise(function(resolve, reject){
+  var osType = require('os');
+  var dirpath = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
+
+		fs.readFile(dirpath, (err, data) => {
+      if (err)  { reject("false")}
+      else {
+				var os = require("os");
+				var text = data.toString().split(os.EOL);
+				console.log( "===============================+> "+ text.length);
+				console.log( "===============================+> "+ text[0]+"===");
+
+        console.log("=data==="+text[0]+"===");
+        resolve(text[0]);
+        
+      }
+    });
+
+	});
 }
 
 
+function FnChk_settingsFile(){
+	var osType = require('os');
+  var dirpath = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
 
-function test1(){
-	return new Promise(function(resolve, reject){
-  	console.log("num1");
-		var uniqid = require('uniqid');
-		var osType = require('os');
-	  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
-		var arg = uniqid()+(new Date()).getTime().toString(36);
-	  
-		if (!fs.existsSync(fileDir)) {
-	    fs.writeFile(fileDir, arg, (err) => {
-	      if(err){
-	        console.log("//== save-dir-path() error  "+ err.message);
-	      }
-				 resolve("async는 Promise방식을 사용합니다.");
-	    });
-	  }else{
-			 resolve("async는 Promise방식을 사용합니다.");
-    }
-
-//    resolve("async는 Promise방식을 사용합니다.");
-  });
+  try{
+   // var retVal =  fs.lstatSync(dirpath).isDirectory();
+	 var retVal = fs.existsSync(dirpath);
+		console.log("FnChk_settingsFile====="+ retVal);
+		return retVal;
+  }catch(e){
+     // Handle error
+     if(e.code == 'ENOENT'){
+       console.log("//==mkdir directory");
+			 return "false";
+     }
+  }
 }
-
-function test2(){
+function userInfoWriteFile(){
   return new Promise(function(resolve, reject){
-    console.log("num2===================");
-		var osType = require('os');
-	  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
-	  const userIndexUUID = fs.readFileSync(fileDir);
-	  console.log("userInfoData====="+ userIndexUUID);
-	  console.log("---------------------------userInfoData====="+ userIndexUUID);
-    resolve(userIndexUUID);
+    var uniqid = require('uniqid');
+    var osType = require('os');
+    var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
+    var arg = uniqid()+(new Date()).getTime().toString(36);
+
+    if (!fs.existsSync(fileDir)) {
+      fs.writeFile(fileDir, arg, (err) => {
+        if(err){
+          reject("error");
+          console.log("//== save-dir-path() error  "+ err.message);
+        }
+         resolve(arg);
+      });
+    }else{
+       resolve(arg);
+    }
   });
 }
 
 function getToken(result){
 	return new Promise((resolve,reject) => {
+	console.log("getTokent------ "+ result);
 		var unirest = require('unirest');
 		unirest.post('http://127.0.0.1:3001/userinfo')
 			.headers('Accept', 'application/json')
@@ -285,196 +322,101 @@ function getToken(result){
 			.end(function (response) {
 				if(response.error){return reject(error)}
 				//return resolve(response.body.ops[0].user_uuid);
-				console.log(JSON.stringify(response.body));
+				console.log("gettoken rest ===== "+ JSON.stringify(response.body));
 				return resolve(response.body);
 			});
 		})
 }
 
-function test3(){
-	var osType = require('os');
-  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
-  let returnVal = "";
 
- var data = fs.readFileSync(fileDir, 'utf8');
-	console.log("adddd====="+ data);
-	getToken(data).then(function (data) {
-	  console.log("then===="+data); // response 값 출력
-	}).catch(function (err) {
-	  console.error("then===err===="+ err); // Error 출력
+function chkUuid(){
+	return new Promise(function(resolve, reject){
+	  var osType = require('os');
+	  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
+	  let returnVal = "";
+	  
+	 // var data = fs.readFileSync(fileDir, 'utf8');a
+		fs.readFile(fileDir, (err, data) => {
+		  if (err)  { reject("false")}
+			else { 
+				resolve("true");
+			  console.log(data);
+			}
+		});
 	});
+}
 
-	//fs.readFile(fileDir, 'utf8', function(err, result){
-  //	if(err) throw err;
-  //    var unirest = require('unirest');
-  //    unirest.post('http://127.0.0.1:3001/userinfo')
-  //      .header('Accept', 'application/json')
-  //      .send({ "userUuid": result })
-  //      .end(function (response) {
-  //        console.log("=======>"+response.body.ops[0].user_uuid);
-  //      }
-	//		   //.end(function (response) {
-	//	     //  return new Promise((resolve, reject) => {
-	//	     //    if(response) {
-	//	     //    		console.log("1-----"+ response.body.ops[0].user_uuid);
-	//			 // 			returnVal = response.body.ops[0].user_uuid;
-	//	     //      	resolve(response.body.ops[0].user_uuid)
-	//	     //    }
-	//	     //    if(error){
-	//	     //      reject(response)
-	//	     //    }
-	//	     //  })
-	//	     //}
-  //    );
-  //});
-
-
-		//var unirest = require('unirest');
-    //unirest.post('http://127.0.0.1:3001/userinfo')
-  	//  .header('Accept', 'application/json')
-	  //  .send({ "userUuid": uuid })
-		//	.end(function (response) {
-    //  	return new Promise((resolve, reject) => {
-    //  	  if(response) {
-		//			console.log("1-----"+ uuid);
-    //  	    resolve(response)
-    //  	  }
-    //  	  if(error){
-		//				console.log("22==="+ uuid);
-    //  	    reject(response)
-    //  	  }
-    //  	})
-		//	}
-    // );
-
+//  es index create 
+function esIndexExists(arg){
+  return new Promise(function(resolve, reject){
+		console.log("esindexcreate============================");
+    var headersOpt = { 
+      "content-type": "application/json",
+    };
+    request({
+        method:'post',
+        url:'http://127.0.0.1:3001/api/indexExists',
+        form: {'userUuid':  arg},
+        headers: headersOpt,
+        json: true,
+      }, async function (error, response, body) {
+          console.log("esIndex====" +body);
+					if(!error){
+						resolve("true");
+					}else{
+						reject("false");
+					}
+					
+      }
+    );
+  });
 }
 
 
-function userInfo(){
+//  watcher dir info 
+function watcherCall(arg){
+  return new Promise(function(resolve, reject){
+		console.log("watcher call===================");
+		var headersOpt = {  
+  	  "content-type": "application/json",
+		};
+		request({
+  	  	method:'post',
+  	    url:'http://127.0.0.1:3001/watcher',
+  	    form: {'userUuid':  arg}, 
+  	    headers: headersOpt,
+  	    json: true,
+  	  }, function (error, response, body) {  
+  	      console.log(body);  
+					if(!error){
+            resolve("true");
+          }else{
+            reject("false");
+          }
 
-	let path = require('path');
-	var uniqid = require('uniqid');
-	//create_userInfoFile(uniqid()+(new Date()).getTime().toString(36));
-
-	var osType = require('os');
-  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
-	var buf = uniqid()+(new Date()).getTime().toString(36);
-	fs.writeFile(fileDir, buf, function(err) {
-	//fs.writeFile(path.join(__dirname, '/sample.txt'), buf, function(err) {
-        if(err) throw err;
-        console.log('쓰기 완료');
-
-        // 콜벡 안에 또 콜벡
-        fs.readFile(fileDir, 'utf8', function(err, result){
-            if(err) throw err;
-            console.log("읽기 : " + result);
-
-						var unirest = require('unirest');
-  					unirest.post('http://127.0.0.1:3001/userinfo')
-  					  .header('Accept', 'application/json')
-  					  .send({ "userId": result })
-  					  .end(function (response) {
-  					    console.log("=======>"+response.body);
-  					  }
-  					);
-
-        });
-    });
-
-
-  //var unirest = require('unirest');
-  //unirest.post('http://127.0.0.1:3001/profile')
-  //  .header('Accept', 'application/json')
-  //  .send({ "userId": $("#userId").val(), "groupNm": $("#groupNm").val() })
-  //  .end(function (response) {
-  //    console.log(response.body);
-  //  }
-  //);
+			}
+		); 
+  });
 }
 
-function watcherCall(){
-
-	//var osType = require('os');
-  //var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
-	//let userInfoData = "";
-  //fs.access(fileDir, error => {
-  //  if (!error) {
-  //      // The check succeeded
-  //       userInfoData = fs.readFileSync(fileDir);
-  //      console.log("userInfoData====="+ userInfoData);
-
-  //  } else {
-  //    console.log("aaa");
-  //      // The check failed
-  //  }
-  //});
-
-	const request=require('request');
-	request('http://127.0.0.1:3001/watcher',function(error, response, body){
-  	if(!error&&response.statusCode==200) {
-    	console.log(body);
-   	}else{
-    	console.log("error----" + error);
-    }
-	});
-
-
-}
 
 //	watcher dir info 
 function create_settingFile(arg){
+  return new Promise(function(resolve, reject){
+    var osType = require('os');
+    var fileDir  = osType.homedir() + '/.config/hamonikr_finder/finder_config';
 
-  var osType = require('os');
-  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/finder_config';
-
-	console.log("====filepath==="+ fileDir);
-  fs.writeFile(fileDir, arg, (err) => {
-    if(err){
-      console.log("//== save-dir-path() error  "+ err.message);
+    if (!fs.existsSync(fileDir)) {
+      fs.writeFile(fileDir, arg, (err) => {
+        if(err){
+          reject("error");
+          console.log("//== save-dir-path() error  "+ err.message);
+        }
+         resolve(arg);
+      });
+    }else{
+       resolve(arg);
     }
   });
 }
 
-// user info uuid 
-function create_userInfoFile(arg){
-
-  var osType = require('os');
-  var fileDir  = osType.homedir() + '/.config/hamonikr_finder/userinfo_config';
-
-	if (!fs.existsSync(fileDir)) {
-  	console.log("====filepath==="+ fileDir);
-  	fs.writeFile(fileDir, arg, (err) => {
-  	  if(err){
-  	    console.log("//== save-dir-path() error  "+ err.message);
-  	  }
-  	});
-	}else{
-		console.log('aaaaaaaaaaaaaaaa');
-		}
-}
-
-
-function FnChk_settingsFile(){
-
-	var osType = require('os');
-	var dirpath = osType.homedir() + '/.config/hamonikr_finder/';
-	
-	try{
-		 fs.lstatSync(dirpath).isDirectory();
-	}catch(e){
-	   // Handle error
-	   if(e.code == 'ENOENT'){
-		   console.log("//==mkdir directory");
-
-			var exec = require('child_process').exec;
-			exec(" mkdir "+dirpath,
-				function (err, stdout, stderr) {
-					console.log('//==stdout: ' + stdout);
-					console.log('//==stderr: ' + stderr);
-					if (err !== null) {
-						console.log('//== mkdir error: ' + err);
-					}
-			});
-	   }
-	}
-}
